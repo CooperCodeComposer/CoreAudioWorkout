7c0c2e8dc82be860393fc21048d30009604dd0bf
Alistair Cooper <acooper@fender.com>
2024-07-08 08:31:48 -0700

    Initial Commit

---
+++ CoreAudioWorkout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreAudioWorkout
+//
+//  Created by Alistair Cooper on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreAudioWorkout/CoreAudioWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreAudioWorkoutApp.swift
+//  CoreAudioWorkout
+//
+//  Created by Alistair Cooper on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreAudioWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
